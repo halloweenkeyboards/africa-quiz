1c4fe94893a7bb85e9ee954c2bef1e84bf1d2dba
Max McCarty <mvmccart@gmail.com>
2015-07-31 22:59:37 -0400

    first map success

--- src/client/app/map/map.controller.js
+++ src/client/app/map/map.controller.js
@@ -3,10 +3,57 @@
 
     angular
         .module('app.map')
-        .controller('Map', Map);
+.controller('Map', function($scope) {
+    
+})
 
-    function Map() {
-        
+.directive('scChoroplethWorldMap', function() {
+
+    function link(scope, element, attrs) {
+      element[0].style.position = 'relative';
+      element[0].style.display = 'block';
+
+      var choropleth = new Datamap({
+        element: element[0],
+        width: 500,
+        height: 300,
+        projection: 'mercator',
+        fills: {
+          defaultFill: "#ABDDA4",
+          authorHasTraveledTo: "#fa0fa0"
+        },
+        data: {
+          USA: {
+            fillKey: "authorHasTraveledTo"
+          },
+          JPN: {
+            fillKey: "authorHasTraveledTo"
+          },
+          ITA: {
+            fillKey: "authorHasTraveledTo"
+          },
+          CRI: {
+            fillKey: "authorHasTraveledTo"
+          },
+          KOR: {
+            fillKey: "authorHasTraveledTo"
+          },
+          DEU: {
+            fillKey: "authorHasTraveledTo"
+          },
+        }
+      });
+
     }
+
+    return {
+      restrict: 'E',
+      link: link,
+      scope: {
+        data: '&'
+      }
+    };
+
+  });
 
 })();
