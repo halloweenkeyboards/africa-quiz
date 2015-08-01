d90246127c1da45b672b972dfa08010b112b4817
Max McCarty <mvmccart@gmail.com>
2015-07-31 23:25:16 -0400

    working on an african projection, installing bootstrap now

--- src/client/app/map/map.controller.js
+++ src/client/app/map/map.controller.js
@@ -13,12 +13,24 @@
               element[0].style.position = 'relative';
               element[0].style.display = 'block';
 
-              var choropleth = new Datamap({
+            var choropleth = new Datamap({
                 element: element[0],
-                width: 500,
+                width: window.width,
                 height: 300,
-                projection: 'mercator',
-              });
+                scope: 'world',
+                // Zoom in on Africa
+                setProjection: function(element) {
+                  var projection = d3.geo.equirectangular()
+                    .center([23, -3])
+                    .rotate([4.4, 0])
+                    .scale(400)
+                    .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
+                  var path = d3.geo.path()
+                    .projection(projection);
+                
+                  return {path: path, projection: projection};
+                },
+            });
 
             }
 
