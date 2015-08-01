93c481d93ea6bc6d5f34009811d0cc77db1f7c57
Max McCarty <mvmccart@gmail.com>
2015-08-01 01:00:17 -0400

    improved projection

--- src/client/app/map/map.controller.js
+++ src/client/app/map/map.controller.js
@@ -16,22 +16,32 @@
             var choropleth = new Datamap({
                 element: element[0],
                 width: window.width,
-                height: 300,
+                height: 500,
                 scope: 'world',
                 // Zoom in on Africa
                 setProjection: function(element) {
                   var projection = d3.geo.equirectangular()
-                    .center([23, -3])
-                    .rotate([4.4, 0])
-                    .scale(400)
+                    .center([23, 0])
+                    .rotate([4.4, -3])
+                    .scale(300)
                     .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
                   var path = d3.geo.path()
                     .projection(projection);
                 
                   return {path: path, projection: projection};
                 },
+              fills: {
+                defaultFill: "#ABDDA4",
+                pink: '#0fa0fa'
+              },
+              data: {
+                'SSD': { fillKey: 'pink' },
+                'CIV': { fillKey: 'pink' }
+              }
             });
-
+            choropleth.bubbles([
+                {name: 'seychelles', latitude: -4.37 , longitude: 55.27, radius: 20, fillKey: 'pink'}
+            ], {});
             }
 
             return {
