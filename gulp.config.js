a415fd8930c97aaec56618c18f1af83027872221
Max McCarty <mvmccart@gmail.com>
2015-07-31 21:58:24 -0400

    added gulp wiredep task

---
+++ gulp.config.js
@@ -0,0 +1,23 @@
+module.exports = function() {
+    var client = './src/client/';
+    var clientApp = client + 'app/';
+    var config = {
+        bower: {
+            json: require('./bower.json'),
+            directory: './bower_components',
+            ignorePath: '../..'
+        },
+        index: client + 'index.html',
+        js: [
+          clientApp + '**/*.module.js',
+          clientApp + '**/*.js'
+        ]
+    };
+    config.getWiredepOptions = function() {
+        var options = {
+            bowerJson: config.bower.json,
+            directory: config.bower.directory,
+            ignorePath: config.bower.ignorePath
+        };
+    };
+};
--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,14 @@ var gulp = require('gulp');
 var $ = require('gulp-load-plugins')({lazy:true});
 
 var config = require('./gulp.config')();
+
+gulp.task('wiredep', function() {
+    var options = config.getWiredepOptions();
+    var wiredep = require('wiredep').stream;
+
+    return gulp
+        .src(config.index)
+        .pipe(wiredep(options))
+        .pipe($.inject(gulp.src(config.js)))
+        .pipe(gulp.dest(config.client));
+});
