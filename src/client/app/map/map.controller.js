015654bbd7b7024d13aa76a99a8493a0f38d4810
Max McCarty <mvmccart@gmail.com>
2015-08-01 01:05:58 -0400

    added some micronations

--- src/client/app/map/map.controller.js
+++ src/client/app/map/map.controller.js
@@ -40,7 +40,9 @@
               }
             });
             choropleth.bubbles([
-                {name: 'seychelles', latitude: -4.37 , longitude: 55.27, radius: 20, fillKey: 'pink'}
+                {name: 'seychelles', latitude: -4.37 , longitude: 55.27, radius: 20, fillKey: 'pink'},
+                {name: 'mauritius', latitude: -20.16 , longitude: 57.52, radius: 20, fillKey: 'pink'},
+                {name: 'sao tome and principe', latitude: 0.33 , longitude: 6.73, radius: 20, fillKey: 'pink'}
             ], {});
             }
 
