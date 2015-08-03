76102f4d512f61be6d4afda150adf03078f5b781
Max McCarty <mvmccart@gmail.com>
2015-08-03 13:09:54 -0400

    changed some styles, added images directory

--- src/client/app/map/map.controller.js
+++ src/client/app/map/map.controller.js
@@ -18,6 +18,9 @@
                 width: window.width,
                 height: 500,
                 scope: 'world',
+                geographyConfig: {                 
+                    borderColor: '#000000'
+                },
                 // Zoom in on Africa
                 setProjection: function(element) {
                   var projection = d3.geo.equirectangular()
@@ -40,9 +43,9 @@
               }
             });
             choropleth.bubbles([
-                {name: 'seychelles', latitude: -4.37 , longitude: 55.27, radius: 20, fillKey: 'pink'},
-                {name: 'mauritius', latitude: -20.16 , longitude: 57.52, radius: 20, fillKey: 'pink'},
-                {name: 'sao tome and principe', latitude: 0.33 , longitude: 6.73, radius: 20, fillKey: 'pink'}
+                {name: 'seychelles', latitude: -4.37 , longitude: 55.27, radius: 10, fillKey: 'pink'},
+                {name: 'mauritius', latitude: -20.16 , longitude: 57.52, radius: 10, fillKey: 'pink'},
+                {name: 'sao tome and principe', latitude: 0.33 , longitude: 6.73, radius: 10, fillKey: 'pink'}
             ], {});
             }
 
