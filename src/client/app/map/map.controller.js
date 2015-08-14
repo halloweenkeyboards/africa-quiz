4239bcfbf9836b6825d2e061e7f9b2c465403edd
Max McCarty <mvmccart@gmail.com>
2015-08-13 20:35:10 -0400

    .mapData now generated with nations array

--- src/client/app/map/map.controller.js
+++ src/client/app/map/map.controller.js
@@ -317,61 +317,12 @@
         }
       ];
 
-      $scope.mapData = {
-          'AGO': { fillKey: 'unGuessed' },
-          'BDI': { fillKey: 'unGuessed' },
-          'BEN': { fillKey: 'unGuessed' },
-          'BFA': { fillKey: 'unGuessed' },
-          'BHR': { fillKey: 'unGuessed' },
-          'BWA': { fillKey: 'unGuessed' },
-          'CAF': { fillKey: 'unGuessed' },
-          'CIV': { fillKey: 'unGuessed' },
-          'CMR': { fillKey: 'unGuessed' },
-          'COD': { fillKey: 'unGuessed' },
-          'COG': { fillKey: 'unGuessed' },
-          'DJI': { fillKey: 'unGuessed' },
-          'DZA': { fillKey: 'unGuessed' },
-          'EGY': { fillKey: 'unGuessed' },
-          'ERI': { fillKey: 'unGuessed' },
-          'ESH': { fillKey: 'unGuessed' },
-          'ETH': { fillKey: 'unGuessed' },
-          'GAB': { fillKey: 'unGuessed' },
-          'GHA': { fillKey: 'unGuessed' },
-          'GIN': { fillKey: 'unGuessed' },
-          'GMB': { fillKey: 'unGuessed' },
-          'GNB': { fillKey: 'unGuessed' },
-          'GNQ': { fillKey: 'unGuessed' },
-          'KEN': { fillKey: 'unGuessed' },
-          'LBR': { fillKey: 'unGuessed' },
-          'LBY': { fillKey: 'unGuessed' },
-          'LSO': { fillKey: 'unGuessed' },
-          'MAR': { fillKey: 'unGuessed' },
-          'MDG': { fillKey: 'unGuessed' },
-          'MLI': { fillKey: 'unGuessed' },
-          'MOZ': { fillKey: 'unGuessed' },
-          'MRT': { fillKey: 'unGuessed' },
-          'MUS': { fillKey: 'unGuessed' },
-          'MWI': { fillKey: 'unGuessed' },
-          'NAM': { fillKey: 'unGuessed' },
-          'NER': { fillKey: 'unGuessed' },
-          'NGA': { fillKey: 'unGuessed' },
-          'RWA': { fillKey: 'unGuessed' },
-          'SDN': { fillKey: 'unGuessed' },
-          'SEN': { fillKey: 'unGuessed' },
-          'SLE': { fillKey: 'unGuessed' },
-          'SOM': { fillKey: 'unGuessed' },
-          'SSD': { fillKey: 'unGuessed' },
-          'SWZ': { fillKey: 'unGuessed' },
-          'TCD': { fillKey: 'unGuessed' },
-          'TGO': { fillKey: 'unGuessed' },
-          'TUN': { fillKey: 'unGuessed' },
-          'TZA': { fillKey: 'unGuessed' },
-          'UGA': { fillKey: 'unGuessed' },
-          'ZAF': { fillKey: 'unGuessed' },
-          'ZMB': { fillKey: 'unGuessed' },
-          'ZWE': { fillKey: 'unGuessed' },
-          'SLD': { fillKey: 'unGuessed' }          
-        };
+      $scope.mapData = {};
+
+      _.each(nations, function(nation) {
+        $scope.mapData[nation.isoId] = { fillKey: 'unGuessed' };
+      });
+
       $scope.islandNations = [  
           {name: 'seychelles', latitude: -4.37 , longitude: 55.27, radius: 10, id: 'SYC', fillKey: 'unGuessed'},
           {name: 'mauritius', latitude: -20.16 , longitude: 57.52, radius: 10, id: 'MUS', fillKey: 'unGuessed'},
